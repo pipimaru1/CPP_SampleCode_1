86dd37d5d1f6a9dbd18483b455d3198fa9c4e58d
Shishido <pipimaru1@gmail.com>
2025-05-06 15:27:18 +0900

    新しいポインタサンプルプロジェクトを追加
    新しいソースファイル「CPP_SamplePointer_1.cpp」を追加し、ポインタと参照に関するサンプルコードを実装しました。また、関連するプロジェクトファイル「CPP_SamplePointer_1.vcxproj」とフィルターファイル「CPP_SamplePointer_1.vcxproj.filters」を作成しました。既存のソリューションファイル「CPP_SampleCode_1.sln」に新プロジェクトを追加し、「CPP_SampleCode_1.cpp」の「main」関数に整数型の最大値に関するコードを追加・修正しました。デバッグおよびリリース構成が設定され、ファイルの整理も行われました。

---
+++ CPP_SamplePointer_1/CPP_SamplePointer_1.cpp
@@ -0,0 +1,69 @@
+﻿//ポインタと参照の参照のサンプル
+#include <conio.h>
+#include <iostream>
+#include <vector>
+
+void printVector(const std::vector<int>& vec);
+void AddDataVector_Addr(std::vector<int>& vec, int _num);
+void AddDataVector_Val(std::vector<int> vec, int _num);
+
+int main()
+{
+	std::vector<int> vec = { 0, 1, 2 };
+	
+	std::cout << "\n///////////////////////////////////////////\n";
+	printVector(vec);
+
+	_getch(); // ユーザーがキーを押すまで待機
+	std::cout << "\n///////////////////////////////////////////\n";
+	std::cout << "関数に値渡しを指定した場合\n";
+	AddDataVector_Val(vec, 5);
+	printVector(vec);
+
+	_getch(); // ユーザーがキーを押すまで待機
+	std::cout << "\n///////////////////////////////////////////\n";
+	std::cout << "関数に参照渡しを指定した場合\n";
+	AddDataVector_Addr(vec, 5);
+	printVector(vec);
+
+}
+
+
+// 引数にconst std::vector<int>&を指定することで、ベクターの参照を渡すことができる
+// &は参照を示す ポインタと参照の回で詳しく説明する
+void printVector(const std::vector<int>& vec)
+{
+	std::cout << "Vector contents: ";
+	// このfor分は範囲for文と言う
+	// これはC++11以降の機能で、C++の標準ライブラリに追加されたもの
+	// vecの要素を1つずつ取り出してvに代入する
+	// 取り出した要素はconst int&で参照渡しされる 値渡しも可能 
+	for (const int& v : vec)
+	{
+		// 参照渡しされた要素を出力する 
+		std::cout << v << " ";
+	}
+
+	std::cout << std::endl; //std::endlは改行を出力する。処理系に左右されない。"\n"はwindows系の改行
+}
+
+void AddDataVector_Addr(std::vector<int>& vec, int _num)
+{
+	int _init = vec.size(); // ベクターのサイズを取得
+	for (int i = 0; i < _num ; ++i)
+	{
+		vec.push_back(i + _init);
+	}
+	return;
+}
+
+void AddDataVector_Val(std::vector<int> vec, int _num)
+{
+	int _init = vec.size(); // ベクターのサイズを取得
+	for (int i = 0; i < _num; ++i)
+	{
+		vec.push_back(i + _init);
+	}
+	return;
+}
+
